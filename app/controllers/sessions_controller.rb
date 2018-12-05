f3bf7389567285621cdd9946e22f820a8af7390c
katsuimasato <masato.lond2@outlook.jp>
2018-12-05 22:20:04 +0900

    ヘッダーにタスク一覧、ユーザ一覧へのリンク及びログアウトを追加、ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # 検索条件を指定して、最初の一件を取得
+    user = User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました！'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    # reset_sessionだと、セッション内の全てのデータを削除する
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました！'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
