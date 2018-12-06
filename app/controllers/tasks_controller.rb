9dd9d936fc97e548531de8086792945a2e2f72e8
katsuimasato <masato.lond2@outlook.jp>
2018-12-06 22:53:46 +0900

    テスト2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   def index
     # ログインユーザに紐づくタスクのみ取得できるようにする(=tasks.where(id: current_user.id))
-    # テスト
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
