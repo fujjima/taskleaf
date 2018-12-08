eada18841adcd542d192f4f2771b3406d003c103
katsuimasato <masato.lond2@outlook.jp>
2018-12-08 12:11:08 +0900

    テストを1件分作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,15 +4,26 @@ describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     before do
       # ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
     end
+
     context 'ユーザーAがログインしている時' do
       before do
         # ユーザーAでログインする
+        # ログイン画面にアクセス
+        visit login_path
+        # メールアドレス、パスワードを入力
+        find_by_id("session-email").set("a@example.com")
+        find_by_id("session-password").set("password")
+        # ログインするボタンの押下
+        click_button 'ログインする'
       end
 
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
