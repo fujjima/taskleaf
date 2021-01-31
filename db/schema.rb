a9c90a0c66aa9a4158c579f4917520bb7e7304e1
katsuimasato <masato.lond2@outlook.jp>
2021-02-01 02:42:18 +0900

    remove elapsed time column from task table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_151141) do
+ActiveRecord::Schema.define(version: 2021_01_31_173859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2021_01_27_151141) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.datetime "finished_at"
-    t.integer "elapsed_time"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
