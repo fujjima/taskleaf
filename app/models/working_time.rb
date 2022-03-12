dd73ce644e413ce36305ffbb5f6099f24c85cc4f
katsuimasato <masato.lond2@outlook.jp>
2022-03-12 12:13:57 +0900

    fix to short liner about working time

--- app/models/working_time.rb
+++ app/models/working_time.rb
@@ -17,7 +17,7 @@ class WorkingTime < ApplicationRecord
   class << self
 
     def working_time_with_task_id_hash
-      WorkingTime.all.group_by(&:task_id).each_with_object({}) do |(task_id, working_times), hash|
+      self.all.group_by(&:task_id).each_with_object({}) do |(task_id, working_times), hash|
         sum_working_times = working_times.sum { |wt| sum_working_time_per_task(wt) }
         hash[task_id] = sum_working_times
         hash
