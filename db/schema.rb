c55a6bb7b890ca8757da32c74fa810cb8728a5b0
katsuimasato <masato.lond2@outlook.jp>
2022-04-17 23:08:58 +0900

    add list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_17_135517) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_140546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_135517) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "board_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -95,6 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_135517) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "lists", "boards"
   add_foreign_key "tags", "users"
   add_foreign_key "task_tags", "tags"
   add_foreign_key "task_tags", "tasks"
